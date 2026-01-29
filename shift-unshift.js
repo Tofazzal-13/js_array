4cafe2b47b71adb19d84bbceda3a8f14cd75850a
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-01-29 18:10:12 +0600

    shift and unshift

---
+++ shift-unshift.js
@@ -0,0 +1,10 @@
+const person = ["rahim", "karim", "jadu","kodu"]
+person.shift(); // it's use to remove frist value in a array 
+console.log(person);
+
+person.unshift("babu","labu")
+
+console.log( person );
+
+
+
