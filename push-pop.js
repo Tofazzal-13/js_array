5b7245e80c976cae7452f76a2e86a664a01b0a77
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-01-29 18:06:09 +0600

    push pop

---
+++ push-pop.js
@@ -0,0 +1,9 @@
+const number = [ 12,54,28,27,641,45,21,46,523];
+console.log(number);
+
+number.push(33,22,44); // add new data or value in array 
+console.log(number);
+
+const out1 = number.pop() // remove the last value in array . if you want you can store remove value in a variable . if any needed you can use this value 
+
+console.log(number);
