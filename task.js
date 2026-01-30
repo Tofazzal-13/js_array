ad5ef2d756621774698fed852648130296a4dfe9
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-01-30 07:15:48 +0600

    task 1 and 2

---
+++ task.js
@@ -0,0 +1,22 @@
+// task 1 
+
+/* const fruits = ["banana", "apple", "lichu", "orange", "jackfruits"];
+console.log(fruits[3]);
+fruits[2] = "alu"
+console.log(fruits);
+ */
+
+// task 2 
+
+/* const touristDestination = ["bandorban","sundorban","kataban"];
+touristDestination.push("cox-bazar");
+console.log(touristDestination);
+touristDestination.push("natore","rajshahi");
+console.log(touristDestination);
+touristDestination.pop()
+console.log(touristDestination);
+ */
+
+
+
+
