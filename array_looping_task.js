8a6c9a57eb05c04408c70aa0f9056f218d34ddc4
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-04 18:42:12 +0600

    array looping task 1 and 2

---
+++ array_looping_task.js
@@ -0,0 +1,30 @@
+// task 1
+
+/* const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+const newColors = [];
+
+for(let color of colors){
+    console.log(color);
+    newColors.unshift(color)
+}
+console.log(newColors); */
+
+
+
+// task 2
+
+/* const numbers = [12, 98, 5, 41, 23, 78, 46];
+const evenNumbers = [];
+
+for(let number of numbers){
+    if(number % 2 === 0){
+        evenNumbers.push(number)
+    }
+}
+
+console.log(evenNumbers); */
+
+
+// task 3
+
+
