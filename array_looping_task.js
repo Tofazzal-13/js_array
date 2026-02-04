116a194e8afe05951ec1d17c6ed7c79b7628729f
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-04 19:04:04 +0600

    array  looping task 3 and 4

--- array_looping_task.js
+++ array_looping_task.js
@@ -27,4 +27,33 @@ console.log(evenNumbers); */
 
 // task 3
 
+var array = ['Tom', 'Tim', 'Tin', 'Tik'];
+let result = "";
+for(let arr of array){
+    // console.log(arr);
+    result += arr;
+}
+// console.log(result);
+
+
+// task 4
+const statement = 'I am a hard working person';
+
+const splitStatement = statement.split(" ");
+
+let newStatement = []
+
+for(let splitS of splitStatement){
+     newStatement.unshift(splitS) 
+}
+
+const newSentence = newStatement.join(" ")
+console.log(newSentence);
+    
+
+
+
+
+
+
 
